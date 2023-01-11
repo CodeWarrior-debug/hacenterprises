b0eee891ffe0de319c03115ab51a3b3f7d7f7851
James Jordan <jpjordan1987@gmail.com>
2023-01-11 10:18:31 -0500

    build successful

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,28 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import { Noto_Sans_Display } from '@next/font/google'
-import "../styles/Hero.module.css"
-import Image from 'next/image'
-import heroImg from '../public/HeroSection.png'
+import { Noto_Sans_Display } from "@next/font/google";
+import "../styles/Hero.module.css";
+import Image from "next/image";
+import heroImg from "../public/HeroSection.png";
 
-const noto = Noto_Sans_Display()
+const noto = Noto_Sans_Display();
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>HAC Enterprises</title>
-        <meta name="description" content="Hoshang Anklesaria Consulting Enterprises, your trusted advisor for commercial security implementations." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" /> //TODO:resize the lock, re-color?
-      </Head>
-      <main className='wrapper'>
-        <Image
-        className={"fill"}
-        src={heroImg}
-        alt="gradient-background"
-        // style="object-fit:cover"
-        
-        
-        
-
+        <meta
+          name="description"
+          content="Hoshang Anklesaria Consulting Enterprises, your trusted advisor for commercial security implementations."
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* TODO:resize the lock, re-color? */}
+      </Head>
+      <main className="wrapper">
+        <Image className={"fill"} src={heroImg} alt="gradient-background" />
       </main>
     </>
-  )
+  );
 }
