f2fd550676fade5dd5dff12248d28ddbf9d3ed9c
James Jordan <jpjordan1987@gmail.com>
2023-01-13 03:01:02 -0500

    started list for core functions

---
+++ public/categories.js
@@ -0,0 +1,40 @@
+
+module.exports={categories}
+const categories=[
+    {
+        icon: "",
+        bg_color:"bg-green",
+        titles_color:"text-green",
+        titleleft: "ARCHITECTURE",
+        titleright: "ARCHITECTURE",
+        p_text:"Our end-to-end architectural solutions rely on through understanding of our client's goals and objectives, state of environment, and challenges. Our primary mission is to create solutions that maximize our clients' ROI through effective coordination and collaboration with stakeholders.",
+        url:"/our_company/our_values"
+    },
+    {
+        icon: "",
+        bg_color:"bg-",
+        titles_color:"text-",
+        titleleft: "",
+        titleright: "",
+        p_text:"",
+        url:""
+    },
+    {
+        icon: "",
+        bg_color:"bg-",
+        titles_color:"text-",
+        titleleft: "",
+        titleright: "",
+        p_text:"",
+        url:""
+    },
+    {
+        icon: "",
+        bg_color:"bg-",
+        titles_color:"text-",
+        titleleft: "",
+        titleright: "",
+        p_text:"",
+        url:""
+    },
+]
