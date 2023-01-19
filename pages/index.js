34d3806f3bfefac500bf4c56f8cd4c366a286aaa
James Jordan <jpjordan1987@gmail.com>
2023-01-19 16:26:34 -0500

    having hard time with next background hero image, this is known issue

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Home = () => {
   return (
     <>
-      <h1>New Stuff!!</h1>
+
+
+<div className='heroWrapper'>
+      <div className='imageWrapper'>
+        <Image
+          priority
+          src="/homepage-header-background-V2.gif"
+          alt="hero image example"
+          width={1000}
+          height={100}
+        />
+      </div>
+      <div className='heroContent'>
+        <h1>Hero Image</h1>
+        <p>Next.js hero image example.</p>
+      </div>
+
+    </div>
+
+
+
     </>
   )
 }
