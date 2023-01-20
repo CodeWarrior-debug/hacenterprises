271be0725c657d83aa07f12f56e2c954bf69f41f
James Jordan <jpjordan1987@gmail.com>
2023-01-20 10:30:10 -0500

    found method for hero background with nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import useDimensions from 'react-cool-dimensions';
+import arrayCeil from '../assets/arrayCeil'
 
-const Home = () => {
-  return (
-    <>
+export default function Hero() {
+  //
+    const [heroImage, setHeroImage] = useState('hero-1920.jpg')
+    const imageSizes = [600, 1280, 1920]
 
+    const { observe, unobserve, width, height, entry } = useDimensions({
+        onResize: ({ observe, unobserve, width, height, entry }) => {
+            setHeroImage(`hero-${arrayCeil(imageSizes, width)}.jpg`)
 
-<div className='heroWrapper'>
-      <div className='imageWrapper'>
-        <Image
-          priority
-          src="/homepage-header-background-V2.gif"
-          alt="hero image example"
-          width={1000}
-          height={100}
-        />
-      </div>
-      <div className='heroContent'>
-        <h1>Hero Image</h1>
-        <p>Next.js hero image example.</p>
-      </div>
+            unobserve(); // To stop observing the current target element
+            observe(); // To re-start observing the current target element
+        },
+    });
 
-    </div>
-
-
-
-    </>
-  )
+    return (
+        <div
+            ref={observe}
+            class="w-full h-screen flex justify-center items-center overflow-hidden relative bg-black myHero">
+            <Image
+                src='/homepage-header-background-V2.gif'
+                alt="Hero Image"
+                className="opacity-60 object-cover object-left-bottom"
+                fill
+            />
+            <div class="flex flex-col justify-center items-center px-3">
+                <h1 class=" text-center text-3xl md:text-5xl text-white font-bold drop-shadow-lg">WELCOME TO <br />
+                    <span class="text-primary">MY COMPANY</span>
+                </h1>
+                <p class="mt-5 text-center text-lg text-white opacity-90">Making tomorrows widgets today...</p>
+                
+            </div>
+        </div>
+    )
 }
-
-export default Home
